a7e0396716add3423393cc736fb582d7e88821f2
rajkumarm95 <rajkumar.murugesan@zysk.tech>
2023-07-13 18:18:10 +0530

    update huskey added

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,11 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin','unused-imports'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:unused-imports/recommended',
   ],
   root: true,
   env: {
@@ -21,5 +22,14 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-  },
+    'no-unused-vars': 'error',
+    'no-unused-imports': 'error',
+    'no-console': 'error',
+    'eqeqeq': 'error',
+    'curly': 'error',
+    'camelcase': 'error',
+    'no-mixed-spaces-and-tabs': 'error',
+    'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 1 }],
+    'prefer-const': 'error'
+  }  
 };
